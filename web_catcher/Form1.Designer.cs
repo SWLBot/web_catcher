9a740030bfb2bee5e384ec90ebd1ae66261ff206
icecat <icecat2012@gmail.com>
2016-09-15 00:53:17 +0800

    icecat2012

---
+++ web_catcher/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace web_catcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.file_textBox = new System.Windows.Forms.TextBox();
+            this.start_button = new System.Windows.Forms.Button();
+            this.display_textBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // file_textBox
+            // 
+            this.file_textBox.AllowDrop = true;
+            this.file_textBox.Location = new System.Drawing.Point(13, 13);
+            this.file_textBox.Name = "file_textBox";
+            this.file_textBox.Size = new System.Drawing.Size(178, 22);
+            this.file_textBox.TabIndex = 0;
+            this.file_textBox.TextChanged += new System.EventHandler(this.file_textBox_TextChanged);
+            // 
+            // start_button
+            // 
+            this.start_button.Location = new System.Drawing.Point(197, 12);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(75, 23);
+            this.start_button.TabIndex = 1;
+            this.start_button.Text = "start";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // display_textBox
+            // 
+            this.display_textBox.Location = new System.Drawing.Point(12, 41);
+            this.display_textBox.Name = "display_textBox";
+            this.display_textBox.Size = new System.Drawing.Size(260, 209);
+            this.display_textBox.TabIndex = 2;
+            this.display_textBox.Text = "";
+            this.display_textBox.TextChanged += new System.EventHandler(this.display_textBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.display_textBox);
+            this.Controls.Add(this.start_button);
+            this.Controls.Add(this.file_textBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox file_textBox;
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.RichTextBox display_textBox;
+    }
+}
+
---
+++ web_catcher/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace web_catcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void file_textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void display_textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private WebBrowser web_connecter = new WebBrowser();
+        private string file_place;
+        private static void system_record(ref RichTextBox box, string words, Color color)
+        {
+            box.Select(box.TextLength, 0);
+            box.SelectionColor = Color.Black;
+            box.AppendText("[" + DateTime.Now.ToString("yyyyMMdd HH:mm:ss") + "] \r\n");
+            box.Select(box.TextLength, 0);
+            box.SelectionColor = color;
+            box.AppendText(words);
+        }
+
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            file_place = file_textBox.Text;
+            if (string.IsNullOrWhiteSpace(file_place))
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.InitialDirectory = "C:";
+                ofd.Filter = "Text Files (*.txt)|*.txt";//|All Files (*.*)|*.*";
+                ofd.Title = "請開啟文字檔案";
+                ofd.CheckFileExists = true;				  //檔案 不存在顯示錯誤訊息
+                ofd.CheckPathExists = true;                 //路徑 不存在顯示錯誤訊息
+                if (ofd.ShowDialog(this) == DialogResult.Cancel)
+                    return;
+                file_textBox.Text = ofd.FileName;
+            }
+            else if (file_place == "clear")
+            {
+                display_textBox.Clear();
+                file_textBox.Clear();
+                return;
+            }
+            else if(File.Exists(file_place))
+            {
+                system_record(ref display_textBox, "Start load\r\n", Color.Blue);
+
+                StreamReader input_txt = new StreamReader(file_place);
+                string target_url = input_txt.ReadLine();
+                input_txt.Close();
+
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(target_url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Stream receiveStream = response.GetResponseStream();
+
+                    if (response.CharacterSet == null)
+                    {
+                        system_record(ref display_textBox, "ERROR : Get NULL string\r\n", Color.Red);
+                        response.Close();
+                        return ;
+                    }
+
+                    StreamReader readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                    document_completed( readStream.ReadToEnd() );
+
+                    response.Close();
+                    readStream.Close();
+                    system_record(ref display_textBox, "End load\r\n", Color.Green);
+                }
+                else
+                {
+                    system_record(ref display_textBox, "Return not ok\r\n", Color.Red);
+                }
+            }
+            else
+            {
+                system_record(ref display_textBox, "ERROR : file is not exists\r\n", Color.Red);
+            }
+        }
+        private void document_completed(string RowWebContent)
+        {
+            StreamWriter output_txt = new StreamWriter(file_place, true, Encoding.UTF8);
+
+            remove_comment(ref RowWebContent);
+            remove_javascript(ref RowWebContent);
+            remove_htmltag(ref RowWebContent);
+            remove_enter_space(ref RowWebContent);
+
+            output_txt.WriteLine("#########################################################");
+            output_txt.WriteLine(RowWebContent);
+            output_txt.Close();
+        }
+        /*
+        private void remove_button(ref string pure_data)
+        {
+            pure_data = Regex.Replace(pure_data, @"<[^>]*?(input\s*type\s*=\s*\"\s*button\s*\")[\s\S]*?>[\s\S]*?<[\s\S]*?>", "");
+        }
+        */
+        private void remove_comment(ref string pure_data)
+        {
+            pure_data = Regex.Replace(pure_data, @"<!--[\s\S]*?-->", "");
+        }
+
+        private void remove_javascript(ref string pure_data)
+        {
+            pure_data = Regex.Replace(pure_data, @"<script[\d\D]*?>[\d\D]*?</script>", "");
+            pure_data = Regex.Replace(pure_data, @"<noscript[\d\D]*?>[\d\D]*?</noscript>", "");
+        }
+
+        private void remove_htmltag(ref string pure_data)
+        {
+            pure_data = Regex.Replace(pure_data, @"<[^>]*>", "");
+        }
+
+        private void remove_enter_space(ref string pure_data)
+        {
+            pure_data = Regex.Replace(pure_data, @"\n[\n\s\t\f]*", "\n");
+        }
+
+
+
+
+
+        /*
+        private void webbrowser_document_completed(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (e.Url.AbsolutePath != (sender as WebBrowser).Url.AbsolutePath)
+                 return; 
+            StreamReader data_in = new StreamReader(web_connecter.DocumentStream, Encoding.UTF8);
+            string RowWebContent = data_in.ReadToEnd();
+            StreamWriter output_txt = new StreamWriter(file_place, true, Encoding.UTF8);
+
+            remove_comment(ref RowWebContent);
+            remove_javascript(ref RowWebContent);
+            remove_htmltag(ref RowWebContent);
+            remove_enter_space(ref RowWebContent);
+
+            output_txt.WriteLine("#########################################################");
+            output_txt.WriteLine(RowWebContent);
+            output_txt.Close();
+        }
+        */
+    }
+}
